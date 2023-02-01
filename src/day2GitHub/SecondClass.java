6f169960b42b33bb5659422a4e734a8e81cc6c23
user <sevgi012127@gmail>
2023-01-31 18:47:15 -0700

    some changes on second class

---
+++ src/day2GitHub/SecondClass.java
@@ -0,0 +1,8 @@
+package day2GitHub;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("I am learning Github");
+        System.out.println("2+2");
+    }
+}
